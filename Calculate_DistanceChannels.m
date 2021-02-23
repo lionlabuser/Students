27d9f78ae81c834300202ce9703e460682f5893d
lionlabuser <64552192+lionlabuser@users.noreply.github.com>
2021-02-23 17:24:46 -0500

    Add formula for calculate distance in arc centered at 0 0 0

--- Calculate_DistanceChannels.m
+++ Calculate_DistanceChannels.m
@@ -17,13 +17,21 @@ for a=1:nchan
         
         ChanDistanceBTW(a,b)=sqrt( (zone.pos(a,1)-zone.pos(b,1))^2 + (zone.pos(a,2)-zone.pos(b,2))^2+ (zone.pos(a,3)-zone.pos(b,3))^2);
         ChanDistanceBTW(b,a)=ChanDistanceBTW(a,b);
+        
+        rayon(a,b)=sqrt( zone.pos(a,1)^2 + zone.pos(a,2)^2 + zone.pos(a,3)^2);
+        rayon(b,a)=rayon(a,b);
+        distanceARC(a,b)=2*rayon(a,b)*asin(ChanDistanceBTW(a,b)/(2*rayon(a,b)));
+        distanceARC(b,a)=distanceARC(a,b);
     end
     ChanDistance(a)=zone.pos(a,4); 
     ChanXYZ(a,:)=zone.pos(a,1:3);
 end
 
+distanceARC=real(distanceARC);
+mrayon=mean(rayon,'all','omitnan');
+
 ChanNum=1:nchan;
 ChanHelp=sprintf('ChanNum = Channel ID (#)\nChanDistance = length of each channel in centimeters (source-detector distance)\nChanDistanceBTW = matrice of distance between the midpoint of one channel to the midpoint of another channel (in cm)\nChanXYZ =  x y z coordinates of each channel (in cm)');
 
-save([dirmtg 'ChannelsDistance.mat'],'Chan*')
+%save([dirmtg 'ChannelsDistance.mat'],'Chan*')
 
