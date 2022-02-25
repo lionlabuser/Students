94dd4a4624b4a231d0ff829bdaa0521e0e5eb01f
kassrog <84396072+kassrog@users.noreply.github.com>
2022-02-25 11:55:25 -0500

    Adapted to artifactDetectionCorrection

--- Physiology_NIRS_LCD/EXAMPLE_AUTORUN_correctionGlobal.m
+++ Physiology_NIRS_LCD/EXAMPLE_AUTORUN_correctionGlobal.m
@@ -3,12 +3,13 @@ clear
 %%%%%%%%%%%%%%%%%% parameter to adjust %%%%%%%%%%%%%%%%%%
 fileXLS = 'C:\data\Malnutrition\Resting\NIRS\DocumentInfo\ReadDataRestMal.xlsx';
 
-FolderAdjustment = 0;
-FilterAUX = 0;
-AUXPHYSIOLOGY = 1; %if you dont want, put 0
-GLOBALPHYSIOLOGY = 0; %if you dont want, put 0
-WHOLEsPCA = 0; %if you dont want, put 0
-VIEWplot = 0; %if you dont want, put 0
+FolderAdjustment = 0; %if you dont want, put 0
+AUX_Artifact = 0;
+AUX_Filter = 0;
+AUX_Regression = 1;
+AUX_SpatialFilter = 0;
+WHOLEsPCA = 0;
+VIEWplot = 0;
 
 %paths={'C:\data\Malnutrition\Resting\NIRS\G10115\DetectAuto\DetectManual\Filter0,01_0,08_dCONC\'};
 %multimodalDirectory= {'C:\data\Malnutrition\Resting\NIRS\Multimodal\'};
@@ -23,14 +24,23 @@ VIEWplot = 0; %if you dont want, put 0
 % zoneFile ={'C:\Data\ELAN\Martine_0m\BB016\GlobalZoneHBO.zone'...
 %     'C:\Data\ELAN\Martine_0m\BB017\GlobalZoneHBO.zone'};
 
-
-% ------------ parameters to adjust for AUXPHYSIOLOGY
+% ------------ parameters to adjust for AUX_ARTIFACT
+jobC.AUX = {'SAT' 'RESP' 'EKG'};
+jobC.threshold = [1.5 1.5 2;... %moving STD threshold, respectively for SAT, RESP & SAT, in standardized units(zscore)
+    .5 .7 .6];%...  %moving AVG threshold, respectively for SAT, RESP & SAT, in standardized units (zscore)
+%               .2 .2 ]; %moving STD DIFFERENCE threshold, respectively for SAT and RESP, in standardized units(zscore)
+jobC.int = [4 1 4];
+jobC.pp = [.99 .99 .99];
+jobC.adjwindow = [1 3.5 1] ; %moving window length before and after X in seconds for each AUX ( *2: if you want a 2 sec window = please write 1)
+jobC.minlength = 1.5; %minimum length of intervals (good or bad) in data points = here is 1.5 sec
+jobC.pausetime = 4;
+% ------------ parameters to adjust for AUX_REGRESSION
 jobA.outAUXfolder = 'filAUX'; %for nirs_run_filterAUX
 jobA.copynirs = 1; %for nirs_run_filterAUX
 jobA.covariables = 'Sat,Resp,EKG'; %for nirs_run_GLM_regressAUX
-jobA.e_NIRSmatdirnewbranch = 'SatRespEKG'; %name of the new branch to create
+jobA.e_NIRSmatdirnewbranch = 'SatRespEKGcorr'; %name of the new branch to create
 
-%------------- parameters for GLOBALPHYSIOLOGY
+%------------- parameters for AUX_SPATIALFILTER
 jobG.trig = [0]; %for nirs_run_GlobalPhysio % 0 for resting OR trig number for task
 jobG.globalavg = 0; %for nirs_run_GlobalPhysio
 jobG.globalpca = 0; %for nirs_run_GlobalPhysio
@@ -85,18 +95,27 @@ for isubject=2:size(raw,1)
             nirs_run_NIRSmatdiradjust(jobF) %run script
         end
         
-        if FilterAUX == 1
-            %% filter AUX
-           jobA.NIRSmat = NIRSmat; %modify            
-           nirs_run_filterAUX(jobA); %run script
+        if AUX_Artifact == 1
+            %% AUX artifact detection & correction
+            jobC.NIRSmat = NIRSmat;
+            jobC.ID = raw{isubject,1};
+            nirs_run_AUX_artifactDetectionCorrection(jobC) %run script
         end
         
-        if AUXPHYSIOLOGY == 1
+        if AUX_Filter == 1
+            %% filter AUX
+            jobA.NIRSmat = NIRSmat; %modify
+            jobA.ID = raw{isubject,1};
+            nirs_run_filterAUX(jobA); %run script
+        end
+        
+        if AUX_Regression == 1
             %%extract AUX regressed data
-            jobA.NIRSmat = NIRSmat; %modify 
+            jobA.NIRSmat = NIRSmat; %modify
+            jobA.ID = raw{isubject,1};
             nirs_run_GLM_regressAUX(jobA); %run script
             
-             %%create new branch
+            %%create new branch
             jobA.m_newbranchcomponent = 1; %1 if copy paste SelectedFactors.mat and CorrectionApply.mat
             jobA2 = nirs_run_NIRSmatcreatenewbranch(jobA); %run script (create new folder + copy NIRS, SelectedFactors)
             dir1 = fileparts(jobA.NIRSmat{1}); %move saved graphs in the newbranch folder
@@ -105,7 +124,7 @@ for isubject=2:size(raw,1)
             tf = contains({flist.name},'Physio');
             for n = 1:nnz(tf)
                 idx = find(tf);
-            movefile(fullfile(flist(idx(n)).folder, flist(idx(n)).name),dir2)
+                movefile(fullfile(flist(idx(n)).folder, flist(idx(n)).name),dir2)
             end
             
             %overwrite nirs data with the corrected data
@@ -114,10 +133,10 @@ for isubject=2:size(raw,1)
             nirs_writeNIR_aftercorr(jobA2);%run script, write hdr & mrk
             
             %%suppress SelectedFactors.mat file in the new directory
-            delete([dir2 filesep 'SelectedFactors.mat'])
+            delete([dir2 filesep 'SelectedFactors.mat']);
         end
         
-        if GLOBALPHYSIOLOGY==1
+        if AUX_SpatialFilter==1
             %% run global physio + extract regressed data
             jobG.physzone = {raw{isubject,izoneaux}};
             jobG.NIRSmat = NIRSmat; %modify
@@ -234,17 +253,15 @@ for isubject=2:size(raw,1)
         raw{isubject,idoit} = 0;
         %catch
         %disp(['Error subject ' raw{isubject,1},' fail'])
-    disp([raw{isubject,1} ' done'])    
+        disp([raw{isubject,1} ' done'])
+        try
+            xlswrite(fileXLS,raw)
+        catch
+            [filepath,name,ext] = fileparts(fileXLS);
+            xlswrite(fullfile(filepath,['ERROR_OPENXLS',name,ext]),raw);
+            disp(['ERROR XLS IS OPEN LOOK AT ', fullfile(filepath,['ERROR_OPENXLS',name,ext])])
+        end
     end
-
-end
-
-try
-    xlswrite(fileXLS,raw)
-catch
-    [filepath,name,ext] = fileparts(fileXLS);
-    xlswrite(fullfile(filepath,['ERROR_OPENXLS',name,ext]),raw);
-    disp(['ERROR XLS IS OPEN LOOK AT ', fullfile(filepath,['ERROR_OPENXLS',name,ext])])
 end
 
 clear
